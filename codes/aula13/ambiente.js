f4d01fe5c55c579dcb7c2d311bb9a2515b9acbed
ryan-raniel <112257612+ryan-raniel@users.noreply.github.com>
2023-07-12 17:27:56 -0300

    aula 13 e 14 concluídas

---
+++ codes/aula13/ambiente.js
@@ -0,0 +1,13 @@
+var c = 1
+
+   /* while (c <= 8) {
+        console.log(`passo ${c}`) //crase 
+        c++
+    }
+    */
+
+
+   do {
+    console.log(`passo ${c}`) //crase
+    c++
+   } while (c <= 6 )
---
+++ codes/aula14/ambiente.js
@@ -0,0 +1,25 @@
+
+/*
+var c = 1
+// #1
+while (c <=5) {
+    console.log(c)
+    c++
+}
+// #2
+do {
+    console.log('Tudo bem?')
+    c++
+} while (c <= 5)
+
+// #3
+for(var c = 1; c <= 5; c++) {
+    console.log(c)
+}
+*/
+
+console.log('Vai começar...')
+for(var c = 1;c <= 4; c++) {
+    console.log(c)
+}
+console.log('FIM!!!')
