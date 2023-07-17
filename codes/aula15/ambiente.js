1c973bb023c0e224363d197a2d19b90666ae31f9
ryan-raniel <112257612+ryan-raniel@users.noreply.github.com>
2023-07-17 14:14:27 -0300

    variaveis Compostas

--- codes/aula15/ambiente.js
+++ codes/aula15/ambiente.js
@@ -12,3 +12,10 @@ console.log(`O primeiro valor do vetor é o ${num[0]}`)
 for(let pos=0; pos<num.length; pos++) {
     console.log(`A posição ${pos} tem o valor ${num[pos]}`)
 }
+
+let pos = num.indexOf(8)
+if (pos == -1) {
+    console.log('O valor não foi encontrado')
+} else {
+    console.log(`O valor 4 está na posição ${pos}`)
+}
--- codes/aula15/vetornatela.js
+++ codes/aula15/vetornatela.js
@@ -1,5 +1,14 @@
 let valores = [8, 1, 7, 4, 2, 9]
 
+valores.sort()
+/*
+for(let pos=0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+*/
+
+//num.indexOf(7) === 3
+
 for(let pos = 0; pos < valores.length; pos++) {
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
